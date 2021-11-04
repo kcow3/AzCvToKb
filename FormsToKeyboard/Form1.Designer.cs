a011d0d551422040d6a51ab21072e498ebcb64f8
cowdr <cowdr@KEVIN-GAME>
2021-11-04 18:13:21 +0200

    Add project files.

---
+++ FormsToKeyboard/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace FormsToKeyboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.InputText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(163, 181);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(75, 23);
+            this.SendBtn.TabIndex = 0;
+            this.SendBtn.Text = "Send";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(12, 181);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 2;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(12, 12);
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(226, 163);
+            this.InputText.TabIndex = 3;
+            this.InputText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 212);
+            this.Controls.Add(this.InputText);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.SendBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.RichTextBox InputText;
+    }
+}
+
